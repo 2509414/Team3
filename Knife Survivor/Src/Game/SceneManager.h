76d1b6491338d24baecbbadf6b3d5a9bdc773171
ru-dade <2409028@s.asojuku.ac.jp>
2026-06-08 11:40:12 +0900

    6/8

--- Knife Survivor/Src/Game/SceneManager.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../../Shot.h"
-//シーン全体の管理（処理用）
-int StepScene();
-
-//シーン全体の管理（描画用）
-void DrawScene();
--- Knife Survivor/Src/Game/SceneResult.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-	//リザルト画面全体の管理（処理用）
-int StepResult();
-	//リザルト画面全体の管理（描画用）
-void DrawResult();
