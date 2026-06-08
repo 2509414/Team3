bb5978e183aab11aab45b617bb6633361b7d6607
ru-dade <2409028@s.asojuku.ac.jp>
2026-06-08 17:54:53 +0900

    6/8
    コントローラー処理完成！

--- Knife Survivor/Src/Lib/Input.cpp
+++ Knife Survivor/Src/Lib/Input.cpp
@@ -3,16 +3,18 @@
 #include "../Game/Knife/Knife.h"
 #include "../Game/Knife/Knife2.h"
 
+
 //キーボード入力構造体
 typedef struct {
 	unsigned int m_nowKey;	//今回入力したキー
 	unsigned int m_preKey;	//前回入力したキー
 }INPUT_DATA;
 
+XINPUT_STATE input;
 static INPUT_DATA g_inputData = { 0 };
-
 extern Knife Knife1;
 extern Knife2 knife2;
+
 //＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 //			キー入力更新
 //＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
@@ -24,43 +26,56 @@ void UpdateKeyInput()
 	//一度データを0にする
 	g_inputData.m_nowKey = 0;
 
-	//Dを押した
-	if (CheckHitKey(KEY_INPUT_D))g_inputData.m_nowKey |= KEY_RIGHT;
-
-	//Aを押した
-	if (CheckHitKey(KEY_INPUT_A))g_inputData.m_nowKey |= KEY_LEFT;
-
-	//Sを押した
-	if (CheckHitKey(KEY_INPUT_S))g_inputData.m_nowKey |= KEY_JUMP;
-
-	//Dかつシフトを押した
-	if (CheckHitKey(KEY_INPUT_D) && CheckHitKey(KEY_INPUT_LSHIFT))g_inputData.m_nowKey |= KEY_DASH;
+	//1P===========================================================================
+	GetJoypadXInputState(DX_INPUT_PAD1, &input);
 	
-	//Aかつシフトを押した
-	if (CheckHitKey(KEY_INPUT_A) && CheckHitKey(KEY_INPUT_LSHIFT))g_inputData.m_nowKey |= KEY_LDASH;
+	//Dを押したまたはコントローラーの右を押したら (右移動)
+	if (CheckHitKey(KEY_INPUT_D)||input.Buttons[XINPUT_BUTTON_DPAD_RIGHT])g_inputData.m_nowKey |= KEY_RIGHT;
 
-	if (CheckHitKey(KEY_INPUT_W) && Knife1.m_isActive == false) g_inputData.m_nowKey |= KEY_KNIFE;
+	//Aを押したまたはコントローラーの左を押したら (左移動)
+	if (CheckHitKey(KEY_INPUT_A)||input.Buttons[XINPUT_BUTTON_DPAD_LEFT])g_inputData.m_nowKey |= KEY_LEFT;
+
+	//Sを押した Aボタンを押した (ジャンプ)
+	if (CheckHitKey(KEY_INPUT_S) || input.Buttons[XINPUT_BUTTON_A]) g_inputData.m_nowKey |= KEY_JUMP;
+
+	//Dかつシフトを押した またはコントローラーの右を押したかつXボタンを押したら (ダッシュ)
+	if (CheckHitKey(KEY_INPUT_D) && CheckHitKey(KEY_INPUT_LSHIFT)||
+		input.Buttons[XINPUT_BUTTON_DPAD_RIGHT] && input.Buttons[XINPUT_BUTTON_X])g_inputData.m_nowKey |= KEY_DASH;
+	
+	//Aかつシフトを押した またはコントローラーの左を押したかつXボタンを押したら (ダッシュ)
+	if (CheckHitKey(KEY_INPUT_A) && CheckHitKey(KEY_INPUT_LSHIFT) ||
+		input.Buttons[XINPUT_BUTTON_DPAD_LEFT] && input.Buttons[XINPUT_BUTTON_X])g_inputData.m_nowKey |= KEY_LDASH;
+	
+	//Wかつナイフの生存フラグがfalse　またはコントローラーのRBを押したかつナイフの生存フラグがfalse (ナイフを投げる)
+	if (CheckHitKey(KEY_INPUT_W) && Knife1.m_isActive == false ||
+		input.Buttons[XINPUT_BUTTON_RIGHT_SHOULDER] && Knife1.m_isActive == false) g_inputData.m_nowKey |= KEY_KNIFE;
 	
 	if(IsMouseLeftClick() == true) g_inputData.m_nowKey |= KEY_ITEMCRAFT;
 
+	//2P===========================================================================
+	GetJoypadXInputState(DX_INPUT_PAD2, &input);
 
-	//Lを押した
-	if (CheckHitKey(KEY_INPUT_L))g_inputData.m_nowKey |= KEY_RIGHT2;
+	//Lを押した またはコントローラーの右を押したら (右移動)
+	if (CheckHitKey(KEY_INPUT_L) || input.Buttons[XINPUT_BUTTON_DPAD_RIGHT])g_inputData.m_nowKey |= KEY_RIGHT2;
 
-	//Jを押した
-	if (CheckHitKey(KEY_INPUT_J))g_inputData.m_nowKey |= KEY_LEFT2;
+	//Jを押した またはコントローラーの左を押したら (左移動)
+	if (CheckHitKey(KEY_INPUT_J) || input.Buttons[XINPUT_BUTTON_DPAD_LEFT])g_inputData.m_nowKey |= KEY_LEFT2;
 
-	//Dかつシフトを押した
-	if (CheckHitKey(KEY_INPUT_L) && CheckHitKey(KEY_INPUT_RSHIFT))g_inputData.m_nowKey |= KEY_DASH2;
+	//Dかつシフトを押した またはコントローラーの右を押したかつXボタンを押したら (ダッシュ)
+	if (CheckHitKey(KEY_INPUT_L) && CheckHitKey(KEY_INPUT_RSHIFT) ||
+		input.Buttons[XINPUT_BUTTON_DPAD_RIGHT] && input.Buttons[XINPUT_BUTTON_X])g_inputData.m_nowKey |= KEY_DASH2;
 
-	//Aかつシフトを押した
-	if (CheckHitKey(KEY_INPUT_J) && CheckHitKey(KEY_INPUT_RSHIFT))g_inputData.m_nowKey |= KEY_LDASH2;
+	//Aかつシフトを押した またはコントローラーの左を押したかつXボタンを押したら (ダッシュ)
+	if (CheckHitKey(KEY_INPUT_J) && CheckHitKey(KEY_INPUT_RSHIFT)||
+		input.Buttons[XINPUT_BUTTON_DPAD_LEFT] && input.Buttons[XINPUT_BUTTON_X])g_inputData.m_nowKey |= KEY_LDASH2;
 
-	//Kを押した
-	if (CheckHitKey(KEY_INPUT_K))g_inputData.m_nowKey |= KEY_JUMP2;
+	//Kを押した Aボタンを押した (ジャンプ)
+	if (CheckHitKey(KEY_INPUT_K)||
+		input.Buttons[XINPUT_BUTTON_A])g_inputData.m_nowKey |= KEY_JUMP2;
 
-	//Iを押した
-	if (CheckHitKey(KEY_INPUT_I) && knife2.m_isActive == false) g_inputData.m_nowKey |= KEY_KNIFE2;
+	//Iを押した またはコントローラーのRBを押したかつナイフの生存フラグがfalse (ナイフを投げる)
+	if (CheckHitKey(KEY_INPUT_I) && knife2.m_isActive == false||
+		input.Buttons[XINPUT_BUTTON_RIGHT_SHOULDER] && Knife1.m_isActive == false) g_inputData.m_nowKey |= KEY_KNIFE2;
 }
 
 int IsKeyInput(unsigned int key)
